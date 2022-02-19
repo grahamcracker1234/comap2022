0e410305603b660d46cd0b71f5710984874a923e
Graham Preston <grahampreston@comcast.net>
2022-02-19 13:38:54 -0500

    Added better debug and fixed portfolio worth bug

--- main.py
+++ main.py
@@ -37,65 +37,74 @@ class Sell(Action):
 class Hold(Action): pass
 
 # Get current currency rates from dataframe. Rate will be numpy.nan if market is closed for that currency and include_nan is True. Use include_nan = False to get the latest valid currency rate even if the market is closed.
-def get_currency_rate(dataframe, include_nan: bool = True):
+def get_currency_rate(dataframe, include_nan=True):
     if include_nan: return Currency(1, dataframe.BTC.values[-1], dataframe.GOLD.values[-1])
     btc = dataframe.BTC.values
+    btc = btc[~np.isnan(btc)]
     gold = dataframe.GOLD.values
-    return Currency(1, btc[~np.isnan(btc)][-1], gold[~np.isnan(gold)][-1])
+    gold = gold[~np.isnan(gold)]
+    if len(btc) > 0 and len(gold) > 0: return Currency(1, btc[-1], gold[-1])
+    return Currency(1, np.nan, np.nan)
+
+# Convert all currency to USD to get estimate of portfolio's worth
+def get_portfolio_worth(dataframe, commission_rate, portfolio):
+    currency_rate = get_currency_rate(dataframe, include_nan=False)
+    action = Sell(currency_rate, commission_rate, Currency(0, portfolio.btc, portfolio.gold))
+    return portfolio.usd + action.usd
 
 # Run the trade simulation
-def simulate(data, commission_rate, wallet, action_model):
+def simulate(data, commission_rate, portfolio, action_model):
     for day in data.index:
         # Only look at past stream of daily prices 
         stream_history = data[:day+1]
         
         # Debug output
-        print("-" * 50)
         print(f"Day {day}")
-        print(wallet)
+        print(f"Portfolio Worth: ${round(get_portfolio_worth(stream_history, commission_rate, portfolio), 2)}")
+        print(f"\tUSD: {portfolio.usd}")
+        print(f"\tBTC: {portfolio.btc}")
+        print(f"\tGOLD: {portfolio.gold}")
+        print()
         
         # Get action based on action model
-        action = action_model(day, stream_history, commission_rate, wallet)
+        action = action_model(day, stream_history, commission_rate, portfolio)
         
-        # Adjust wallet to lock in the results of the chosen action
+        # Adjust portfolio to lock in the results of the chosen action
         if isinstance(action, Buy):
-            wallet.usd -= action.usd
-            wallet.btc += action.btc
-            wallet.gold += action.gold
+            portfolio.usd -= action.usd
+            portfolio.btc += action.btc
+            portfolio.gold += action.gold
         elif isinstance(action, Sell):
-            wallet.usd += action.usd
-            wallet.btc -= action.btc
-            wallet.gold -= action.gold
+            portfolio.usd += action.usd
+            portfolio.btc -= action.btc
+            portfolio.gold -= action.gold
 
-    # Convert all currency to USD to get estimate of portfolio's worth
-    currency_rate = get_currency_rate(data, include_nan=False)
-    action = Sell(currency_rate, commission_rate, Currency(0, wallet.btc, wallet.gold))
-    return wallet.usd + action.usd
+    return get_portfolio_worth(data, commission_rate, portfolio)
 
 # Buy both and hold
-def null_action(day, stream_history, commission_rate, wallet) -> Action:
+def null_action(day, stream_history, commission_rate, portfolio) -> Action:
     currency_rate = get_currency_rate(stream_history)
-    if np.isnan(currency_rate.btc) or np.isnan(currency_rate.gold) or wallet.usd == 0: return Hold()
-    return Buy(currency_rate, commission_rate, Currency(0, wallet.usd / 2, wallet.usd / 2))
+    if np.isnan(currency_rate.btc) or np.isnan(currency_rate.gold) or portfolio.usd == 0: return Hold()
+    return Buy(currency_rate, commission_rate, Currency(0, portfolio.usd / 2, portfolio.usd / 2))
 
 # Buy bitcoin and hold
-def null_action_btc(day, stream_history, commission_rate, wallet) -> Action:
+def null_action_btc(day, stream_history, commission_rate, portfolio) -> Action:
     currency_rate = get_currency_rate(stream_history)
-    if wallet.usd == 0: return Hold()
-    return Buy(currency_rate, commission_rate, Currency(0, wallet.usd, 0))
+    if portfolio.usd == 0: return Hold()
+    return Buy(currency_rate, commission_rate, Currency(0, portfolio.usd, 0))
     
 # Buy gold and hold
-def null_action_gold(day, stream_history, commission_rate, wallet) -> Action:
+def null_action_gold(day, stream_history, commission_rate, portfolio) -> Action:
     currency_rate = get_currency_rate(stream_history)
-    if wallet.usd == 0: return Hold()
-    return Buy(currency_rate, commission_rate, Currency(0, 0, wallet.usd))    
+    if portfolio.usd == 0: return Hold()
+    return Buy(currency_rate, commission_rate, Currency(0, 0, portfolio.usd))    
 
 def main():
     data = pd.read_csv("data/full.csv")
     commission_rate = Currency(1, 0.02, 0.01)
-    wallet = Currency(1000, 0, 0)
+    portfolio = Currency(1000, 0, 0)
     
-    portfolio_worth = simulate(data, commission_rate, wallet, null_action)
+    portfolio_worth = simulate(data, commission_rate, portfolio, null_action)
     print(f"Total USD: ${round(portfolio_worth, 2)}")
     
 main()
